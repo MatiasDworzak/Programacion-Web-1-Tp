0a3e0cdc9c5040257fe2cc841915852b066e9339
LuketoDev <lucasbravouni@gmail.com>
2024-11-05 11:43:31 -0300

    comentarios agregados en el js de item del index

--- js/items-index.js
+++ js/items-index.js
@@ -3,19 +3,19 @@ const articulos = document.querySelectorAll('.articulo-categoria');
 
 articulos.forEach(articulo => {
 
-    articulo.addEventListener('mouseenter', () => {
+    articulo.addEventListener('mouseenter', () => { // mouse encima del item
         articulo.classList.add("articulo-categoria-mouseenter");
         });
 
-    articulo.addEventListener('mouseleave', () => {
+    articulo.addEventListener('mouseleave', () => { // mouse cuando se va de encima del item
         articulo.classList.remove("articulo-categoria-mouseenter");
         });
 
-    articulo.addEventListener('mousedown', () => {
+    articulo.addEventListener('mousedown', () => { // mouse cuando mantengo presionado el item
         articulo.classList.add("articulo-categoria-mousedown");
         });
 
-    articulo.addEventListener('mouseup', () => {
+    articulo.addEventListener('mouseup', () => { // mouse cuando dejo de presionar el item
         articulo.classList.remove("articulo-categoria-mousedown");
         });
 });
